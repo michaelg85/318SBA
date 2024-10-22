c71cfc12711f562d47d0bc9f3e8c2f89d7da89b1
Michael <Magawrych85@gmail.com>
2024-10-22 13:31:02 -0400

    setup sever file

--- server.mjs
+++ server.mjs
@@ -1,1 +1,25 @@
+//Imports
 import express from "express"
+
+//Setup
+const app = express();
+let PORT = process.env.PORT || 3001;
+
+
+//DB Connection
+
+
+
+//Middleware
+
+
+
+//Routes
+
+
+
+//Listener
+app.listen(PORT, ()=> {
+    console.log(`Server is running on PORT: ${PORT}`);
+});
+
