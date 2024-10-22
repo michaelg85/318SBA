bf5867d663485b08c28e2692858803b13839afa8
Michael <Magawrych85@gmail.com>
2024-10-22 13:55:32 -0400

    added data file

---
+++ data/data.mjs
@@ -0,0 +1,29 @@
+let movies = [
+  {
+    name: "The Dark Knight Rises",
+    genre: "action",
+    year: 2012,
+  },
+  {
+    name: "The Joker",
+    genre: "thriller",
+    year: 2019,
+  },
+  {
+    name: "Life is Beautiful",
+    genre: "drama",
+    year: 1997,
+  },
+  {
+    name: "Vegas Vacation",
+    genre: "comedy",
+    year: 1997,
+  },
+  {
+    name: "Home Alone",
+    genre: "Christmas",
+    year: 1990,
+  },
+];
+
+export default movies;
