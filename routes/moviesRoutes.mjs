a26c43eafda1cc950a868b23804b685f1c5a530a
Michael <Magawrych85@gmail.com>
2024-10-24 22:22:15 -0400

    updated routes

--- routes/moviesRoutes.mjs
+++ routes/moviesRoutes.mjs
@@ -2,13 +2,36 @@ import express from "express";
 import movies from "../data/data.mjs";
 const router = express.Router();
 
+router
+    .route('/')
+    .post((req, res) => {
+        if (req.body.name && req.body.genre && req.body.year) {
+            let newMovie = {
+                id: movies.length +1,
+                name: req.body.name,
+                genre: req.body.genre,
+                year: req.body.year,
+            };
+
+            movies.push(newMovie);
+
+            res.render('show', newMovie);
+        } else {
+            res.send('Incorrect Info');
+        }
+    })
+    .get((req, res) => {
+        let options = {
+            allMovies: movies,
+        };
+        res.render('showAll', options);
+    });
 
 
 
 
 
-
-
+ 
 
 
 
